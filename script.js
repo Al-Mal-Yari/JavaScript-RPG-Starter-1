44f0186bc4bb35347ba702b6be316f926236077a
Al Mal-yari <72258580+Al-Mal-Yari@users.noreply.github.com>
2024-05-23 20:32:01 +0000

    fixed an error = error daw sa line 40-48(nalimutan lang mag-add ng brackets sa dulo)

--- script.js
+++ script.js
@@ -37,12 +37,12 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
-  button1.innerText = location["button text"[0];
-  button2.innerText = location["button text"[1];
-  button3.innerText = location["button text"[2];
-  button1.onclick = location["button functions"[0];
-  button2.onclick = location["button functions"[1];
-  button3.onclick = location["button functions"[2];
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
   text.innerText = location.text;
 }
 
