da5fc761c9f30321f3e72ad4b6c5e4456b6f28c8
Al Mal-yari <72258580+Al-Mal-Yari@users.noreply.github.com>
2024-05-23 21:32:58 +0000

    created function sellWeapon and its properties

--- script.js
+++ script.js
@@ -119,6 +119,17 @@ function buyWeapon() {
     }
   }
 }
+
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon = inventory.shift();
+    text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory.join(", ");
+  }
+}
+
 function fightSlime(){
   
 }
