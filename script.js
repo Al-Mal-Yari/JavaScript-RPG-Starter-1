ff0a17cfece3a21babe5845581a22b649e901ea3
Allaine T. T. <18630490-Allaine-TT@users.noreply.replit.com>
2024-05-23 19:26:43 +0000

    added elements with sorts of IDs and buttons

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ let monsterHealth;
 // inventory = nilagay sa loob ng array yung mga items which is yung double bracket
 let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterNameText = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
