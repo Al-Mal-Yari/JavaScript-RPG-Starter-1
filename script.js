c08d0122315c3fa018da969c88647f4a1934af3a
Al Mal-yari <72258580+Al-Mal-Yari@users.noreply.github.com>
2024-05-23 19:31:38 +0000

    initialize buttons - goStore, goCave, fightDragon

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let fighting;
 let monsterHealth;
 // inventory = nilagay sa loob ng array yung mga items which is yung double bracket
 let inventory = ["stick"];
+
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
@@ -17,3 +18,8 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterNameText = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
