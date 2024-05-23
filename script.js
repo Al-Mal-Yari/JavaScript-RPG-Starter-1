23e91d50f2581f28093f68dc7fcb7757f4cbf5ea
Al Mal-yari <72258580+Al-Mal-Yari@users.noreply.github.com>
2024-05-23 21:49:48 +0000

    added fight on const locations and its properties and updated function fightSlime, fightBeast, fightDragon

--- script.js
+++ script.js
@@ -73,6 +73,12 @@ const locations = [{
     "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
     "button functions": [fightSlime, fightBeast, goTown],
     text: "You enter the cave. You see some monsters."
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   }
 ]
 
@@ -149,14 +155,21 @@ function sellWeapon() {
   }
 }
 
-function fightSlime(){
-  
+function fightSlime() {
+  fighting = 0;
+  goFight();
 }
 
 function fightBeast(){
-  
+  fighting = 1;
+  goFight();
 }
 
 function fightDragon() {
-  console.log("Fighting dragon.");
+  fighting = 2;
+  goFight();
+}
+
+function goFight() {
+  
 }
