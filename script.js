341c9f45002875333ff2f970926f2cb77a0803ca
Allaine T. T. <18630490-Allaine-TT@users.noreply.replit.com>
2024-05-23 19:18:06 +0000

    comment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*it's better to declare all of the variables sa una palang */
 let xp = 0;
 let hp = 100;
 let gold = 50;
@@ -5,3 +6,4 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+const button1 = document.querySelector("#button1");
