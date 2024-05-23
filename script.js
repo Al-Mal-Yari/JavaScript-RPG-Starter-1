e82cbd16e5ae55b10ad69546531df939055d8bba
Al Mal-yari <72258580+Al-Mal-Yari@users.noreply.github.com>
2024-05-23 21:30:37 +0000

    function buyWeapon updated properties

--- script.js
+++ script.js
@@ -78,18 +78,18 @@ function goTown() {
   update(locations[0]);
   //kaya may [0] to only choose the first object in the array which is yung "locations"
 }
-
 function goStore() {
   update(locations[1]);
 }
 function goCave(){
   update(locations[2]);
 }
-function fightDragon(){
-  console.log("Fighting dragon.")
+function fightDragon() {
+  console.log("Fighting dragon.");
 }
+
 function buyHealth() {
-  if(gold >= 10){
+  if(gold >= 10) {
     gold -= 10;
     hp += 10;
     goldText.innerText = gold;
@@ -98,8 +98,26 @@ function buyHealth() {
     text.innerText = "You do not have enough gold to buy health.";
   }
 }
-function buyWeapon(){
-  
+function buyWeapon() {
+  if(currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory.join(", ");
+    } 
+    else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
+    else {
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+      button2.onclick = sellWeapon;
+    }
+  }
 }
 function fightSlime(){
   
