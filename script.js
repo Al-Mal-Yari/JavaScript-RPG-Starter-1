9a5a5b207b30b47bd1d4ed8d2cee671f0fef0728
Al Mal-yari <72258580+Al-Mal-Yari@users.noreply.github.com>
2024-05-23 20:03:43 +0000

    fixed an error (the function goStore has no brackets - returns an error)

--- script.js
+++ script.js
@@ -23,14 +23,14 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
-function goStore()
+function goStore() {
   button1.innerText = "Buy 10 Health (10 gold)";
   button2.innerText = "Buy Weapon (30 gold)";
   button3.innerText = "Go to town square";
   button1.onclick = buyHealth;
   button2.onclick = buyWeapon;
   button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+  text.innerText = "You enter the store."; }
 function goCave(){
   console.log("Going to cave.")
 }
